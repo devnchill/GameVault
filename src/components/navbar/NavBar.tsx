d03bdda55faa6afd8c882ace6b1d9b8806db4ba5
0xDev <169875752+devnchill@users.noreply.github.com>
2025-07-08 23:06:50 +0530

    feat: implemented basic navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,18 +1,39 @@
+import style from "./NavBar.module.css";
 import { Link } from "react-router-dom";
+import { MdLightMode, MdDarkMode } from "react-icons/md";
+import { useEffect, useState } from "react";
+
 const NavBar = () => {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  useEffect(() => {
+    document.body.classList.toggle("dark", theme === "dark");
+  }, [theme]);
+
+  const ThemeIcon = theme === "light" ? MdDarkMode : MdLightMode;
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">GameVault</Link>
-        </li>
-        <li>
-          <Link to="Store">Store</Link>
-        </li>
-        <li>
-          <Link to="Cart">Cart</Link>
-        </li>
-      </ul>
+    <nav className={`${style.flex_between} ${style.navbar}`}>
+      <div>
+        <Link to="/">GameVault</Link>
+      </div>
+      <div className={style.flex_between}>
+        <ul className={`${style.flex_between} ${style.ul}`}>
+          <li>
+            <Link to="/store">Store</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+          <li>
+            <ThemeIcon className={style.react_icons} onClick={toggleTheme} />
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
